e58d91c2a04191894fb11b2d6b3d047af64fdaa8
Kevin Sparks <kevin@just-digital.net>
2023-07-20 03:29:36 +1200

    aider: Import the JSON Forms library.
    # Context:
    USER: Import the JSON Forms library
    ASSISTANT: To import the JSON Forms library, we need to modify the `App.js` file.
    
    Here is the change:
    
    ```javascript
    questionnaire/src/App.js
    <<<<<<< ORIGINAL
    import React, { Component } from 'react';
    import logo from './logo.svg';
    import './App.css';
    =======
    import React, { Component } from 'react';
    import logo from './logo.svg';
    import './App.css';
    import { JsonForms } from '@jsonforms/react';
    >>>>>>> UPDATED
    ```
    
    This change adds the import statement for the JSON Forms library at the top of the file.

--- questionnaire/src/App.js
+++ questionnaire/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { JsonForms } from '@jsonforms/react';
 
 class App extends Component {
   render() {
